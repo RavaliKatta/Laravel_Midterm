dcbb00b52f778eaad4c5288f897128e4032a3294
Ravali <rk694@njit.edu>
2018-11-02 18:46:14 -0700

    Fix: Created a unit test to update the car in the database

---
+++ tests/Unit/UpdateCarTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit;
+use App\Car;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UpdateCarTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $car = Car::find(1);
+        $car->year = 2000;
+        $this->assertTrue($car->save());
+    }
+}
